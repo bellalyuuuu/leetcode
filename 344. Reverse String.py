d0f2d2a6289bcb68c0782f1d8ccad54d6159ba5a
hui lyu <huilyu3@illinois.edu>
2021-04-25 20:31:04 -0400

    Finished Reverse String

---
+++ 344. Reverse String.py
@@ -0,0 +1,28 @@
+'''
+Write a function that reverses a string. The input string is given as an array of characters s.
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+'''
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            temp = s[l]
+            s[l] = s[r]
+            s[r] = temp
+            l += 1
+            r -= 1
+#Time complexity : O(N) to swap N/2N/2 element.
+#Space complexity : O(1), it's a constant space solution.
